431fb29802137039ca03279e0fa4a09505343eb2
John Rayes <live627@gmail.com>
2015-06-09 21:42:19 -0700

    unused function

--- install2.php
+++ install2.php
@@ -52,84 +52,11 @@ if (!array_key_exists('db_add_column', $smcFunc))
 			)
 		),		
 		//End Table
-					
 	);	
 
-	//Creating Tables
 	foreach ($tables as $table)
 	{
-		$table_name = $table['name'];
-		$smcFunc['db_create_table']('{db_prefix}' . $table_name, $table['columns'], $table['indexes']);		
-		$currentTable = $smcFunc['db_table_structure']('{db_prefix}' . $table_name);
-		// Check that all columns are in
-		foreach ($table['columns'] as $id => $col)
-		{
-			$exists = false;
-			// TODO: Check that definition is correct
-			foreach ($currentTable['columns'] as $col2)
-			{
-				if ($col['name'] === $col2['name'])
-				{
-					$exists = true;
-					break;
-				}
-			}
-
-			// Add missing columns
-			if (!$exists)
-				$smcFunc['db_add_column']('{db_prefix}' . $table_name, $col);
-
-			//Check, not change anything?
-			if($exists)
-			{
-				$smcFunc['db_change_column']('{db_prefix}' . $table_name, $col['name'], $col);
-			}
-
-		}
-		//End add missing columns
-		// Check that all indexes are in and correct
-		if ($table['indexes'] > 0)
-		{
-			foreach ($table['indexes'] as $id => $index)
-			{
-				$exists = false;
-	
-				foreach ($currentTable['indexes'] as $index2)
-				{
-					// Primary is special case
-					if ($index['type'] == 'primary' && $index2['type'] == 'primary')
-					{
-						$exists = true;
-	
-						if ($index['columns'] !== $index2['columns'])
-						{
-							$smcFunc['db_remove_index']('{db_prefix}' . $table_name, 'primary');
-							$smcFunc['db_add_index']('{db_prefix}' . $table_name, $index);
-						}
-	
-						break;
-					}
-					// Make sure index is correct
-					elseif (isset($index['name']) && isset($index2['name']) && $index['name'] == $index2['name'])
-					{
-						$exists = true;
-	
-						// Need to be changed?
-						if ($index['type'] != $index2['type'] || $index['columns'] !== $index2['columns'])
-						{
-							$smcFunc['db_remove_index']('{db_prefix}' . $table_name, $index['name']);
-							$smcFunc['db_add_index']('{db_prefix}' . $table_name, $index);
-						}
-	
-						break;
-					}
-				}
-	
-				if (!$exists)
-					$smcFunc['db_add_index']('{db_prefix}' . $table_name, $index);
-			}
-		}
-		//End check indexes
+		$smcFunc['db_create_table']('{db_prefix}' . $table['name'], $table['columns'], $table['indexes'], array(), 'update');
 	}
 
 if (!empty($ssi))
